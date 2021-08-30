30ce7cf50c290803c79a909bd3f35722d7d713fb
pvpvpvpvp <wmrqj@naver.com>
2021-08-30 16:25:02 +0900

    프로그래머스 위클리 5주차 다른코드 리뷰

--- src/com/weekproammers/week05.java
+++ src/com/weekproammers/week05.java
@@ -1,16 +1,17 @@
 package com.weekproammers;
 
 public class week05 {
-
+	 //미리 모든 경우의 수를 구할 경우 가능한 숏코딩
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 int answer = 0, per = 3905;
 		 String word= "EIOA";
-		for(String s : word.split("")) 
+		for(String s : word.split(""))  //word를 하나씩 빼서
 		{
 			System.out.print("AEIOU".indexOf(s)+" ");
 			System.out.println(s);
-			answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
+			answer += "AEIOU".indexOf(s) * (per /= 5) + 1; //answer 에 AEIOU 순서의 인덱스와 같은 s의 정수값*(모든 경우의 수/=5 [n번째 자리에서의 경우의수 n이 +1될때마다 /5으로 줄어든다] )
+//																+1[값 보정]
 		}
 		System.out.println(answer);
 	}
